f760b150c86d3e789525a6ab17f3d158ae2c6b26
masaki <ikeda.masaki@gmail.com>
2017-01-03 21:09:56 +0900

    [add] create server skeleton file

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+
+	//	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/grpclog"
+
+	//	"github.com/golang/protobuf/proto"
+
+	pb "github.com/ikmski/mark2-server/proto"
+)
+
+var (
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile   = flag.String("cert_file", "testdata/server1.pem", "The TLS cert file")
+	keyFile    = flag.String("key_file", "testdata/server1.key", "The TLS key file")
+	jsonDBFile = flag.String("json_db_file", "testdata/route_guide_db.json", "A json file containing a list of features")
+	port       = flag.Int("port", 10000, "The server port")
+)
+
+func main() {
+
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		grpclog.Fatalf("failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+
+	if *tls {
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			grpclog.Fatalf("Failed to generate credentials %v", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+
+	grpcServer := grpc.NewServer(opts...)
+
+	pb.RegisterMessageServiceServer(grpcServer, newServer())
+
+	grpcServer.Serve(lis)
+}
---
+++ server.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	pb "github.com/ikmski/mark2-server/proto"
+	"golang.org/x/net/context"
+)
+
+type messageServer struct {
+}
+
+func newServer() *messageServer {
+	s := new(messageServer)
+	return s
+}
+
+func (s *messageServer) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResult, error) {
+
+	return new(pb.LoginResult), nil
+}
+
+func (s *messageServer) Logout(ctx context.Context, token *pb.AccessToken) (*pb.Result, error) {
+
+	return new(pb.Result), nil
+}
+
+func (s *messageServer) GetUserInfoList(ctx context.Context, token *pb.AccessToken) (*pb.UserInfoListResult, error) {
+
+	return new(pb.UserInfoListResult), nil
+}
+
+func (s *messageServer) GetRoomInfoList(ctx context.Context, token *pb.AccessToken) (*pb.RoomInfoListResult, error) {
+
+	return new(pb.RoomInfoListResult), nil
+}
+
+func (s *messageServer) GetOwnUserInfo(ctx context.Context, token *pb.AccessToken) (*pb.UserInfoResult, error) {
+
+	return new(pb.UserInfoResult), nil
+}
+
+func (s *messageServer) GetOwnRoomInfo(ctx context.Context, token *pb.AccessToken) (*pb.RoomInfoResult, error) {
+
+	return new(pb.RoomInfoResult), nil
+}
+
+func (s *messageServer) CreateRoom(ctx context.Context, req *pb.CreateRoomRequest) (*pb.RoomInfoResult, error) {
+
+	return new(pb.RoomInfoResult), nil
+}
+
+func (s *messageServer) JoinRoom(ctx context.Context, req *pb.JoinRoomRequest) (*pb.RoomInfoResult, error) {
+
+	return new(pb.RoomInfoResult), nil
+}
+
+func (s *messageServer) MatchRandom(ctx context.Context, req *pb.MatchRequest) (*pb.RoomInfoResult, error) {
+
+	return new(pb.RoomInfoResult), nil
+}
+
+func (s *messageServer) ExitRoom(ctx context.Context, token *pb.AccessToken) (*pb.Result, error) {
+
+	return new(pb.Result), nil
+}
+
+func (s *messageServer) SendMessage(ctx context.Context, req *pb.MessageRequest) (*pb.Result, error) {
+
+	return new(pb.Result), nil
+}
+
+func (s *messageServer) SendStream(srv pb.MessageService_SendStreamServer) error {
+
+	return nil
+}
+
+func (s *messageServer) WaitMessage(token *pb.AccessToken, srv pb.MessageService_WaitMessageServer) error {
+
+	return nil
+}
