c2611ae8a26c45e8870f96783da2276c76c043bf
masaki <ikeda.masaki@gmail.com>
2017-03-26 16:05:43 +0900

    [add] create storage struct

---
+++ storage.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
+
+type storage struct {
+	mapData map[string]string
+	setData map[string]([]string)
+}
+
+func newStorage() *storage {
+	s := new(storage)
+	s.mapData = make(map[string]string)
+	s.setData = make(map[string]([]string))
+	return s
+}
+
+/* 確認 */
+func (s *storage) has(key string) bool {
+	return s.hasMap(key) || s.hasSet(key)
+}
+
+/* 削除 */
+func (s *storage) del(key string) error {
+
+	m := new(sync.Mutex)
+	m.Lock()
+	defer m.Unlock()
+
+	var err error
+
+	if s.hasMap(key) {
+		err = s.delMap(key)
+		if err != nil {
+			return err
+		}
+	}
+
+	if s.hasSet(key) {
+		err = s.delSet(key)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+/* 取得 */
+func (s *storage) get(key string) (string, error) {
+
+	val, ok := s.mapData[key]
+	if !ok {
+		err := errors.New(fmt.Sprintf("%s not found\n", key))
+		return "", err
+	}
+
+	return val, nil
+}
+
+/* 設定 */
+func (s *storage) set(key string, value string) error {
+
+	m := new(sync.Mutex)
+	m.Lock()
+	defer m.Unlock()
+
+	s.mapData[key] = value
+
+	return nil
+}
+
+/* メンバー取得 */
+func (s *storage) members(key string) ([]string, error) {
+
+	val, ok := s.setData[key]
+	if !ok {
+		err := errors.New(fmt.Sprintf("%s not found\n", key))
+		return nil, err
+	}
+
+	return val, nil
+}
+
+/* 要素の追加 */
+func (s *storage) add(key string, value string) error {
+
+	m := new(sync.Mutex)
+	m.Lock()
+	defer m.Unlock()
+
+	set, ok := s.setData[key]
+	if !ok {
+		set = make([]string, 0)
+	}
+
+	set = append(set, value)
+	s.setData[key] = set
+
+	return nil
+}
+
+/* 要素の削除 */
+func (s *storage) remove(key string, value string) error {
+
+	set, ok := s.setData[key]
+	if !ok {
+		err := errors.New(fmt.Sprintf("%s not found\n", key))
+		return err
+	}
+
+	m := new(sync.Mutex)
+	m.Lock()
+	defer m.Unlock()
+
+	newSet := make([]string, len(set))
+	i := 0
+	for _, v := range set {
+		if value != v {
+			newSet[i] = v
+			i++
+		}
+	}
+
+	delete(s.setData, key)
+	s.setData[key] = newSet
+
+	return nil
+}
+
+func (s *storage) hasMap(key string) bool {
+	_, ok := s.mapData[key]
+	return ok
+}
+
+func (s *storage) hasSet(key string) bool {
+	_, ok := s.setData[key]
+	return ok
+}
+
+func (s *storage) delMap(key string) error {
+	has := s.hasMap(key)
+	if has {
+		delete(s.mapData, key)
+		return nil
+	} else {
+		return errors.New(fmt.Sprintf("%s not found\n", key))
+	}
+}
+
+func (s *storage) delSet(key string) error {
+	has := s.hasSet(key)
+	if has {
+		delete(s.setData, key)
+		return nil
+	} else {
+		return errors.New(fmt.Sprintf("%s not found\n", key))
+	}
+}
---
+++ storage_test.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestStorageMapData(t *testing.T) {
+
+	storage := newStorage()
+
+	var key = "test_key"
+	var val = "test_val"
+
+	// Has
+	has := storage.has(key)
+	if has {
+		t.Errorf("got %v\nwant %v", has, false)
+	}
+
+	// Set
+	err := storage.set(key, val)
+	if err != nil {
+		t.Errorf("got %v\n", err)
+	}
+
+	// Has
+	has = storage.has(key)
+	if !has {
+		t.Errorf("got %v\nwant %v", has, true)
+	}
+
+	// Get
+	v, err := storage.get(key)
+	if err != nil {
+		t.Errorf("got %v\n", err)
+	}
+	if v != val {
+		t.Errorf("got %v\nwant %v", v, val)
+	}
+
+	// Del
+	err = storage.del(key)
+	if err != nil {
+		t.Errorf("got %v\n", err)
+	}
+
+	// Has
+	has = storage.has(key)
+	if has {
+		t.Errorf("got %v\nwant %v", has, false)
+	}
+}
+
+func TestStorageSetData(t *testing.T) {
+
+	storage := newStorage()
+
+	var key = "test_key"
+	var val01 = "test_val_01"
+	var val02 = "test_val_02"
+	var val03 = "test_val_03"
+
+	// Has
+	has := storage.has(key)
+	if has {
+		t.Errorf("got %v\nwant %v", has, false)
+	}
+
+	// Add
+	err := storage.add(key, val01)
+	if err != nil {
+		t.Errorf("got %v\n", err)
+	}
+	err = storage.add(key, val02)
+	if err != nil {
+		t.Errorf("got %v\n", err)
+	}
+
+	err = storage.add(key, val03)
+	if err != nil {
+		t.Errorf("got %v\n", err)
+	}
+
+	// Has
+	has = storage.has(key)
+	if !has {
+		t.Errorf("got %v\nwant %v", has, true)
+	}
+
+	// Members
+	v, err := storage.members(key)
+	if err != nil {
+		t.Errorf("got %v\n", err)
+	}
+
+	if v[0] != val01 {
+		t.Errorf("got %v\nwant %v", v[0], val01)
+	}
+	if v[1] != val02 {
+		t.Errorf("got %v\nwant %v", v[1], val02)
+	}
+	if v[2] != val03 {
+		t.Errorf("got %v\nwant %v", v[2], val03)
+	}
+
+	// Remove
+	err = storage.remove(key, val02)
+	if err != nil {
+		t.Errorf("got %v\n", err)
+	}
+
+	v, err = storage.members(key)
+	if err != nil {
+		t.Errorf("got %v\n", err)
+	}
+
+	if v[0] != val01 {
+		t.Errorf("got %v\nwant %v", v[0], val01)
+	}
+	if v[1] != val03 {
+		t.Errorf("got %v\nwant %v", v[1], val03)
+	}
+
+	// Del
+	err = storage.del(key)
+	if err != nil {
+		t.Errorf("got %v\n", err)
+	}
+
+	// Has
+	has = storage.has(key)
+	if has {
+		t.Errorf("got %v\nwant %v", has, false)
+	}
+}
