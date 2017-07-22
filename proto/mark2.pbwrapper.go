66f9e9295b0400c4c8de1fa567a933fb6c73923e
masaki <ikeda.masaki@gmail.com>
2017-07-22 14:12:23 +0900

    [update] add groupID in RoomInfo

--- proto/mark2.pbwrapper.go
+++ proto/mark2.pbwrapper.go
@@ -1,20 +1,39 @@
 package mark2
 
+// ===== Model =====
+func NewAccessToken() *AccessToken {
+	at := new(AccessToken)
+	return at
+}
+
 func NewUserInfo() *UserInfo {
 	ui := new(UserInfo)
 	return ui
 }
 
+func NewUserInfoList() *UserInfoList {
+	uil := new(UserInfoList)
+	return uil
+}
+
+func NewRoomInfo() *RoomInfo {
+	ri := new(RoomInfo)
+	return ri
+}
+
+func NewRoomInfoList() *RoomInfoList {
+	ril := new(RoomInfoList)
+	return ril
+}
+
+// ===== Request =====
+
+// ===== Response =====
 func NewResult() *Result {
 	r := new(Result)
 	r.Code = ResultCodes_NG
 	r.Message = ""
 	return r
-}
-
-func NewAccessToken() *AccessToken {
-	at := new(AccessToken)
-	return at
 }
 
 func NewLoginResult() *LoginResult {
