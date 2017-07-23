d917ac58a724104bef9e350789bca274f50826e9
masaki <ikeda.masaki@gmail.com>
2017-07-23 20:45:03 +0900

    WIP room test

---
+++ room_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+
+	mark2 "github.com/ikmski/mark2-server/proto"
+)
+
+func TestRoomCreateRoom(t *testing.T) {
+
+	var groupID uint32 = 1001
+	var capacity uint32 = 3
+
+	room, err := createRoom(groupID, capacity)
+	if err != nil {
+		t.Errorf("got %v\n", err)
+	}
+	if room.info.GroupId != groupID {
+		t.Errorf("got %v\nwant %v", room.info.GroupId, groupID)
+	}
+	if room.info.Capacity != capacity {
+		t.Errorf("got %v\nwant %v", room.info.Capacity, capacity)
+	}
+	if room.info.Status != mark2.RoomStatus_CLOSED {
+		t.Errorf("got %v\nwant %v", room.info.Status, mark2.RoomStatus_CLOSED)
+	}
+
+}
